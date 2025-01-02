595b4e3614ca48136c34affbbfb07916a249c0d2
hugo9442 <hugo9442@gmail.com>
2025-01-02 10:41:21 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,37 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+const contenido = document.getElementById("contenido");
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function getexcuses() {
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let randomNumber = Math.random();
+    let action = ["ate", "peed", "crushed", "broke"];
+    let randomNumber1 = Math.random();
+    let what = ["my homework", "my phone", "the car"];
+    let randomNumber2 = Math.random();
+    let when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
+    let randomNumber3 = Math.random();
+
+    return (
+      who[Math.floor(randomNumber * 3)] +
+      "  " +
+      action[Math.floor(randomNumber1 * 3)] +
+      "  " +
+      what[Math.floor(randomNumber2 * 2)] +
+      "  " +
+      when[Math.floor(randomNumber3 * 4)]
+    );
+  }
+  contenido.innerHTML = getexcuses();
 };
+
+console.log("Hello Rigo from the console!");
